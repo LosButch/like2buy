7d871eeb2a6258c08dd7be1df5a687f9322dfdc0
Troels Gorm Rasmussen <dr@mmj.dk>
2017-11-20 20:31:40 +0100

    Fixed encoding

--- catalog/language/en-gb/account/success.php
+++ catalog/language/en-gb/account/success.php
@@ -1,9 +1,9 @@
-<?php
+ï»¿<?php
 // Heading
-$_['heading_title'] = 'Din Konto er blevet oprettet!';
+$_['heading_title'] = 'Din Konto er oprettet!';
 
 // Text
-$_['text_message']  = '<p>Tillykke! Din nye konto er blevet oprettet!</p> <p>Du kan nu udnytte medlemsrettigheder til at forbedre din online shopping oplevelse hos os.</p> <p>Hvis du har nogle spørgsmål om driften af denne netbutik, kan du altid kontakte os.</p> <p>En bekræftelse er blevet sendt til den angivne e-mail-adresse. Hvis du ikke har modtaget det inden for en time, venligst <a href="%s">kontakt os</a>.</p>';
-$_['text_approval'] = '<p>Tak fordi du har registreret dig hos %s!</p><p>Du vil blive underrettet via e-mail, når din konto er blevet aktiveret af Like2Buy.</p><p>Hvis du har nogle spørgsmål om driften af Like2Buy, vær venlig at<a href="%s">kontakt os</a>.</p>';
+$_['text_message']  = '<p>Tillykke! Din konto er blevet oprettet!</p> <p>Hvis du har nogle spÃ¸rgsmÃ¥l kan du finde svar pÃ¥ det meste inde pÃ¥ <a href="https://like2buy.dk/faq">HjÃ¦lp</a>, ellers er du velkommen til at kontakte os.</p> <p>En mail bekrÃ¦ftelse er blevet sendt til den angivne e-mail-adresse. Hvis du ikke har modtaget det inden for en time, venligst <a href="%s">kontakt os</a>.</p>';
+$_['text_approval'] = '<p>Tak fordi du har registreret dig hos %s!</p><p>Du vil blive underrettet via e-mail, nÃ¥r din konto er blevet aktiveret af Like2Buy.</p><p>Hvis du har nogle spÃ¸rgsmÃ¥l om driften af Like2Buy, vÃ¦r venlig at<a href="%s">kontakte os</a>.</p>';
 $_['text_account']  = 'Konto';
 $_['text_success']  = 'Success';
